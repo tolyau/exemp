8390d5ae1acb510d25f13f8149f2647684c02fdf
Tolik <xiaohei@list.ru>
2023-08-30 22:43:12 +0800

    Маша

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи имя: ");
+string userN = Console.ReadLine();
+
+if (userN == "Маша")
+{
+    Console.WriteLine("Ура Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userN);
+}
